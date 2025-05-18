f8dac64a700099b0f530a1c424f7f74212e417b7
sazanka-ikhsan <ikhsan.muharipan@sazankahenig.com>
2025-05-18 20:31:55 +0700

    feat: - add download helper - setup program.cs and add dependencies

--- Program.cs
+++ Program.cs
@@ -1,27 +1,41 @@
-using AegisLabs_Employee_ExcelToPdfApp.Components;
+﻿using AegisLabs_Employee_ExcelToPdfApp.Components;
+using AegisLabs_Employee_ExcelToPdfApp.Services;
+using Rotativa.AspNetCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddRazorComponents()
     .AddInteractiveServerComponents();
+builder.Services.AddHttpClient();
+
+builder.Services.AddHttpClient("ServerAPI", client =>
+{
+    client.BaseAddress = new Uri("https://localhost:7139/");
+});
+
+// Register services
+builder.Services.AddScoped<IEmployeeService, EmployeeService>();
+builder.Services.AddControllersWithViews();
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
+var rotativaPath = Path.Combine(Directory.GetCurrentDirectory(), "RotativaTool");
+RotativaConfiguration.Setup(rotativaPath);
+
 if (!app.Environment.IsDevelopment())
 {
     app.UseExceptionHandler("/Error", createScopeForErrors: true);
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
 
 app.UseHttpsRedirection();
-
 app.UseStaticFiles();
 app.UseAntiforgery();
 
 app.MapRazorComponents<App>()
     .AddInteractiveServerRenderMode();
+
+app.MapDefaultControllerRoute();
 
 app.Run();
