b2c76486424039879264b1e825bdd13d5e6d3bac
sazanka-ikhsan <ikhsan.muharipan@sazankahenig.com>
2025-05-18 22:48:28 +0700

    feat: - add employee repository interface - add employee service interface - add report service interface

---
+++ Services/Interfaces/IEmployeeService.cs
@@ -0,0 +1,14 @@
+﻿using AegisLabs_Employee_ExcelToPdfApp.Models;
+
+namespace AegisLabs_Employee_ExcelToPdfApp.Services.Interfaces
+{
+    public interface IEmployeeService
+    {
+        Task<List<Employee>> GetAllAsync();
+        Task AddAsync(Employee emp);
+        Task UpdateAsync(Employee emp);
+        Task DeleteAsync(int id);
+    }
+
+
+}
---
+++ Services/Interfaces/IReportService.cs
@@ -0,0 +1,10 @@
+﻿using AegisLabs_Employee_ExcelToPdfApp.Models;
+
+namespace AegisLabs_Employee_ExcelToPdfApp.Services.Interfaces
+{
+    public interface IReportService
+    {
+        Task<byte[]> GenerateExcelAsync(List<Employee> employees);
+        Task<byte[]> GeneratePdfAsync(List<Employee> employees);
+    }
+}
