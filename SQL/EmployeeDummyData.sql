3df86e5920c5921696d210949aa1bb8e033875be
sazanka-ikhsan <ikhsan.muharipan@sazankahenig.com>
2025-05-19 07:35:00 +0700

    doc: added sql initial script including get all CTE

---
+++ SQL/EmployeeDummyData.sql
@@ -0,0 +1,4 @@
+﻿INSERT INTO Employees (Name, Email, Phone, Address) VALUES
+('John Doe', 'john@example.com', '+6281111111', 'Jakarta'),
+('Jane Smith', 'jane@example.com', '+6282222222', 'Bandung'),
+('Alice Johnson', 'alice@example.com', '+6283333333', 'Surabaya');
---
+++ SQL/StoredProcedureCTE.sql
@@ -0,0 +1,43 @@
+﻿------------GET ALL EMPLOYEE ------------------
+CREATE PROCEDURE GetAllEmployees
+AS
+BEGIN
+    WITH EmployeeCTE AS (
+        SELECT Id, Name, Email, Phone, Address FROM Employees
+    )
+    SELECT * FROM EmployeeCTE
+END;
+
+------------ADD EMPLOYEE------------------
+CREATE PROCEDURE AddEmployee
+    @Name NVARCHAR(100),
+    @Email NVARCHAR(100),
+    @Phone NVARCHAR(50),
+    @Address NVARCHAR(255)
+AS
+BEGIN
+    INSERT INTO Employees (Name, Email, Phone, Address)
+    VALUES (@Name, @Email, @Phone, @Address)
+END
+
+------------ UPDATE EMPLOYEE ---------------
+CREATE PROCEDURE UpdateEmployee
+    @Id INT,
+    @Name NVARCHAR(100),
+    @Email NVARCHAR(100),
+    @Phone NVARCHAR(50),
+    @Address NVARCHAR(255)
+AS
+BEGIN
+    UPDATE Employees
+    SET Name = @Name, Email = @Email, Phone = @Phone, Address = @Address
+    WHERE Id = @Id
+END
+
+----------- DELETE EMPLOYEE -------------
+CREATE PROCEDURE DeleteEmployee
+    @Id INT
+AS
+BEGIN
+    DELETE FROM Employees WHERE Id = @Id
+END
